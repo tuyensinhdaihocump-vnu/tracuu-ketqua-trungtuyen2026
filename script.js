b1f911a7112cc596229be5313d211398414e6509
tuyensinhdaihocump-vnu <tuyensinhdaihoc.ump@gmail.com>
2026-08-10 14:42:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 ========================================== */
 
 const API_URL =
-    "https://script.google.com/macros/s/AKfycbxYLHX8NjrsXvbpws0ueYThj9tN-wds7YWBg1rTvcezllFDTAa4q7tjZoPhuRk9vqc/exec";
+    "https://script.google.com/macros/s/AKfycbx0NKScDpvPGepjnkhGGED1ZAYbi7JBSaEqaUR2Oo3Nz2TwcMfJQlIG9QPn3QR1ItY/exec";
 
 
 /* ==========================================
